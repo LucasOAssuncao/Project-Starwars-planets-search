d794a8e8d6378f49b84533b0722e79ad6d7c6255
Lucas Otoni <lotoni.vivo@gmail.com>
2022-07-08 18:10:55 -0300

    requisito 1 completo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
+import DataProvider from './context/DataContext';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <div>
+      <DataProvider>
+        <Table />
+      </DataProvider>
+    </div>
   );
 }
 
